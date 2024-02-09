386c4d6f0efedd7d60236ff26c1406555159f26a
Dylan Montgomery <dmont025@odu.edu>
2024-02-09 14:06:15 -0500

    edit method playGame()

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+pScore = 0;
+cScore = 0;
+turn = 1;
 
 function getComputerChoice() {
     r = Math.floor(Math.random()*3) + 1;
@@ -18,24 +21,45 @@ function playRound(playerSelection, computerSelection) {
     } 
     if (ps==="rock") {
         if (cs==="paper") {
+            cScore++;
             return "Paper beats rock. Computer wins!";
         } else if (cs==="scissors") {
+            pScore++;
             return "Rock beats scissors. Player wins!";
         }
     } else if (ps==="paper") {
         if (cs==="scissors") {
+            cScore++;
             return "Scissors beat rock. Computer wins!";
         } else if (cs==="rock") {
+            pScore++;
             return "Paper beats rock. Player wins!";
         }
     } else if (ps==="scissors") {
         if (cs==="rock") {
+            cScore++;
             return "Rock beats scissors. Computer wins!";
         } else if (cs==="paper") {
+            pScore++;
             return "Scissors beat paper. Player wins!";
         }
     }
 }
 
-const playerChoice = "Rock";
-console.log(playRound(playerChoice,getComputerChoice()))
+function playGame() {
+
+    while (turn < 6) {
+        let player = prompt("Enter a selection (rock, paper, scissors):");
+        comp = getComputerChoice();
+        playRound(player,comp);
+        turn++;
+    }
+
+    return `Score: \nPlayer ${pScore}\nComputer ${cScore}`;
+    if (pScore > cScore) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+console.log(playGame())
